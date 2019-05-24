11b6008453777426418406beb10a8ded520cd635
曳 <oooo@users.noreply.github.com>
2019-05-24 23:13:21 +0800

    oooo

--- src/views/user/admin/OrdinaryList.vue
+++ src/views/user/admin/OrdinaryList.vue
@@ -17,12 +17,12 @@
 				<template slot-scope="scope">
 					<el-button
 							size="mini"
-							@click="handleEdit(scope.$index, scope.row)">Edit
+							@click="handleEdit(scope.$index, scope.row)">编辑
 					</el-button>
 					<el-button
 							size="mini"
 							type="danger"
-							@click="handleDelete(scope.$index, scope.row)">Delete
+							@click="handleDelete(scope.$index, scope.row)">删除
 					</el-button>
 				</template>
 			</el-table-column>

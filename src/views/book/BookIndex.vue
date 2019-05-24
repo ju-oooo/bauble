146e3917b80fa0fb014ee1ce57a250cb5d2a671c
web <j_duang@163.com>
2019-05-24 20:58:13 +0800

    初始化项目

---
+++ src/views/book/BookIndex.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-container>
+    <Menu></Menu>
+    <el-main>
+      <Header></Header>
+      <router-view/>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  import ManageMenu from '@/views/ManageMenu'
+  import Header from '@/components/include/Header'
+  import Footer from '@/components/include/Footer'
+
+  export default {
+    name: "BookIndex",
+    components: {Header, Footer, ManageMenu}
+  }
+</script>
+
+<style scoped>
+  .el-main {
+    padding: 0;
+  }
+</style>

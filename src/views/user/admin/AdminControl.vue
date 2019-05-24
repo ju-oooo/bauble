39ead7aeb16aff665cf3174342a4c45c63c965e4
web <j_duang@163.com>
2019-05-24 08:31:21 +0800

    初始化项目

---
+++ src/views/user/admin/AdminControl.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-main>
+      adminControl 管理员用户 数据信息
+    </el-main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "AdminControl",
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/user/ordinary/OrdinaryControl.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<el-main>
+			OrdinaryControl 普通用户 数据信息
+		</el-main>
+	</div>
+</template>
+
+<script>
+    export default {
+        name: "OrdinaryControl"
+    }
+</script>
+
+<style scoped>
+
+</style>
